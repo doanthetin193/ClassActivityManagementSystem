93cdbeb681b5ed03c43e5825fb52d83d78c5e611
Doan The Tin <tinakiosio@gmail.com>
2026-03-21 21:42:52 +0700

    Allow production environment file to be tracked in git

---
+++ qnu_fe/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiURL: "https://clownfish-app-nfk4m.ondigitalocean.app/AMQNU/api/",
+  socketURL: "wss://clownfish-app-nfk4m.ondigitalocean.app/AMQNU/api/ws/notifications",
+};
